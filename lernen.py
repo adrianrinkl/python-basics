87adb1f457d4277ac95cf4f1e3d8b096fc6e7256
adrianrinkl <adrianrinkl@gmx.de>
2026-03-12 21:56:01 -0700

    aenderungen

--- lernen.py
+++ lernen.py
@@ -1,1 +1,10 @@
-print ("Hello, World!")
+alter = 20
+
+if alter < 18:
+    print("Du bist minderjährig")
+elif alter < 30:
+    print("Du bist jung und volljährig")
+elif alter < 60:
+    print("Du bist mittleren Alters")
+else:
+    print("Du bist ein Senior")
